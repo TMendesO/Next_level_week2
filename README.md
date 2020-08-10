1f4877537e63b18ee15532861f8db8e988760bd2
Thiago Mendes <37598914+TMendesO@users.noreply.github.com>
2020-08-10 18:32:00 -0300

    10-08-2020

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # Next_level_week
+Olá, o que está fazendo? Meu nome é Thiago Mendes e esse projeto foi desenvolvido na semana do imersão do Rocketseat.
 
-  Olá! Meu nome é Thiago Mendes e esse projeto foi 
-desenvolvido na semana da imersão da Rocketseat.
-
-
+Foi um imrsão intenso, a impressão quando acabava cada dia era de que eu estava realmente submerso em um mar de conhecimento. Aprendi muito em cada segundo 🚀🧙 🧑 💻
--- src/pages.js
+++ src/pages.js
@@ -28,17 +28,16 @@ async function pageStudy(req, res) {
 
 
     const query = ` 
-        SELECT classes.*,proffys.*
+         SELECT classes.*, proffys.*
         FROM proffys
         JOIN classes ON (classes.proffy_id = proffys.id)
-        WHERE EXISTS(
-           SELECT class_schedule.*
-           FROM class_schedule
-           WHERE class_schedule.class_id = classes.id
-           AND class_schedule.weekday = ${filters.weekday}
-           AND class_schedule.time_from <= ${timeToMinutes}
-           AND class_schedule.time_to > ${timeToMinutes}
-
+        WHERE EXISTS (
+            SELECT class_schedule.*
+            FROM class_schedule
+            WHERE class_schedule.class_id = classes.id
+            AND class_schedule.weekday = ${filters.weekday}
+            AND class_schedule.time_from <= ${timeToMinutes}
+            AND class_schedule.time_to > ${timeToMinutes}
         )
     AND classes.subject = '${filters.subject}'
 `
